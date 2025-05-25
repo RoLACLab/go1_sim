abb4df07dc47727691f8c1553227e5d62da9cd35
scakki <sakki@mtu.edu>
2025-05-25 18:15:03 -0400

    topic ln setup with noise_gen package

--- src/noise_gen/src/lidar_noise.cpp
+++ src/noise_gen/src/lidar_noise.cpp
@@ -1,21 +1,33 @@
 #include <ros/ros.h>
-#include "std_msgs/String.h"
+#include <sensor_msgs/LaserScan.h>
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+ros::Publisher pub;
+
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  sensor_msgs::LaserScan modified_scan = *msg;
+  ROS_INFO("[ScanCorruptor] Received scan: Ranges size=%lu, Angle min=%.3f, max=%.3f", 
+          modified_scan.ranges.size(),
+          modified_scan.angle_min,
+          modified_scan.angle_max);
+
+  for(int i = 0; i < 50; ++i) {
+    modified_scan.ranges[i] = 1;
+  }
+
+  pub.publish(modified_scan);
 }
 
 int main(int argc, char* argv[])
 {
-    ros::init(argc, argv, "ln");
-    ros::NodeHandle nh;
-    ROS_INFO("Test");
-    ros::Subscriber sub = nh.subscribe("/scan", 1000, chatterCallback);
-    ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
-    
-    ros::spin();
-    
-    return 0;
+  ros::init(argc, argv, "ln");
+  ros::NodeHandle nh;
+  
+  // Initialize publisher with the correct message type
+  pub = nh.advertise<sensor_msgs::LaserScan>("/scan_corrupt", 1000);
+  ros::Subscriber sub = nh.subscribe("/scan", 1000, scanCallback);
+  
+  ros::spin();
+  return 0;
 }
 
--- src/noise_gen/src/talker.cpp
+++ src/noise_gen/src/talker.cpp
@@ -44,8 +44,7 @@ int main(int argc, char **argv)
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("
-  ", 1000);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("", 1000);
 
   ros::Rate loop_rate(10);
 
